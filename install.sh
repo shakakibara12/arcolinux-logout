24eb276e615edf02bb7af3986ab06e3efb04d069
shakaki12 <evilpopcorn321@hotmail.com>
2021-09-12 20:20:38 +0530

    should work now

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ echo "##############################################"
 echo "Installing shaka-logout"
 echo "##############################################"
 
-cp -r usr/share/shakalogout/ /usr/share/shakalogout/
-cp -r usr/share/shakalogout-themes/ /usr/share/shakalogout-themes/
-cp -r usr/local/bin/shaka-logout /usr/local/bin/shaka-logout
+sudo cp -r usr/share/shakalogout/ /usr/share/shakalogout/
+sudo cp -r usr/share/shakalogout-themes/ /usr/share/shakalogout-themes/
+sudo cp -r usr/local/bin/shaka-logout /usr/local/bin/shaka-logout
+cp -r etc/shakalogout.conf $HOME/.config/shakalogout/
--- uninstall.sh
+++ uninstall.sh
@@ -4,5 +4,6 @@ echo "###########################################"
 echo "         Uninstalling shaka-logout         "
 echo "###########################################"
 
-rm -r /usr/share/shakalogout* 2>/dev/null
-rm -r /usr/local/bin/shaka-logout 2>/dev/null
+sudo rm -r /usr/share/shakalogout* 2>/dev/null
+sudo rm -r /usr/local/bin/shaka-logout 2>/dev/null
+rm -r $HOME/.config/shakalogout/
