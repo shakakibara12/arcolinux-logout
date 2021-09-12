84541a521de5cbee4ebbaecf9c7665f26517b3da
shakakibara <evilpopcorn321@gmail.com>
2021-09-12 14:32:07 +0530

    changed name to shaka

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "##############################################"
+echo "Installing shaka-logout"
+echo "##############################################"
+
+cp usr/share/shakalogout/ /usr/share/shakalogout/
+cp usr/share/shakalogout-themes/ /usr/share/shakalogout-themes/
+cp usr/local/bin/shakalinux-logout /usr/local/bin/shakalinux-logout
